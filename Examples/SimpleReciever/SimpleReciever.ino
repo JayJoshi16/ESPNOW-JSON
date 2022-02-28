fb27aeb724462a800fb29b1c24a0687d887c7b86
JAY JOSHI <86542830+JayJoshi16@users.noreply.github.com>
2022-02-28 18:43:37 +0530

    Update SimpleReciever.ino

--- Examples/SimpleReciever/SimpleReciever.ino
+++ Examples/SimpleReciever/SimpleReciever.ino
@@ -1,35 +1,38 @@
+/*  Codes that Recieves JSON data using ESPNOW Protocol
+
+*   Developed by  Jay Joshi 
+*   github.com/JayJoshi16
+*/
+
 
 #include <esp_now.h>
 #include <WiFi.h>
 uint8_t broadcastAddress[] = {0x3C, 0x71, 0xBF, 0x4C, 0x9C, 0xD0};  //Source ESP32's MAC Address
+
 #include <ArduinoJson.h>
 String jsondata;
 DynamicJsonDocument doc(1024);
 
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  //memcpy(&jsondata, incomingData, sizeof(jsondata));
-  char* buff = (char*) incomingData;
-  jsondata = String(buff);
-  Serial.print("Recieved ");
-  Serial.println(jsondata);
-  DeserializationError error = deserializeJson(doc, jsondata);
-  if (!error) {
 
-    long a   = doc["a"]; //values are 1 or 0
-    const char* b  =   doc["b"];//values are "ON" or "OFF" 
-    Serial.println(a);
-    Serial.println(b);
+  char* buff = (char*) incomingData;        //char buffer
+  jsondata = String(buff);                  //converting into STRING
+  Serial.print("Recieved ");
+  Serial.println(jsondata);                 //Complete JSON data will be printed here
+  DeserializationError error = deserializeJson(doc, jsondata);
+    
+  if (!error) {
+        long a   = doc["a"]; 
+        const char* b  =   doc["b"]; 
+        Serial.println(a);                   //values of a
+        Serial.println(b);                   //values of b
   }
 
   else {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    return;
+        Serial.print(F("deserializeJson() failed: "));  //Just in case of an ERROR of ArduinoJSon
+        Serial.println(error.f_str());
+        return;
   }
 
 }
@@ -38,22 +41,15 @@ void setup() {
 
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
+    
+  if (esp_now_init() != ESP_OK) {       //Init ESPNOW
+        Serial.println("Error initializing ESP-NOW");
+        return;
   }
 
-  esp_now_register_recv_cb(OnDataRecv);
-  esp_now_register_send_cb(OnDataSent);
-  esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
+  esp_now_register_recv_cb(OnDataRecv);         //Reciever CallBack function
 
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
-    return;
-  }
+    
 }
 
 void loop() {
